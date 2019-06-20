fabd96ec4bd6db36cc29e8354401ca5b3ebec11e
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2019-06-21 00:43:30 +0900

    Initial Commit

---
+++ HiratsukaTest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HiratsukaTest
+//
+//  Created by 平塚俊輔 on 2019/06/21.
+//  Copyright © 2019 平塚俊輔. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
